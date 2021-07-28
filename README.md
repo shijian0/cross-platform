4acc539921197a1159cbab3d266f76bf9de0d3e6
Rake Yang <fenglaijun@gmail.com>
2021-07-28 15:43:47 +0800

    React Native

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # cross-platform
 跨平台开发技术
+
+## React-Native 0.64.2
+
+安装必须的依赖
+
+- node（推荐使用brew install node安装）
+- watchman
+- xcode
+- cocoapods
+- yarn
+
+运行
+
+```bash
+cd RNPRoject
+yarn ios
+或者open ios/RNProject.xcworkspace 编译调试
+```
+
+## flutter
+
+待续
---
+++ RNProject/App.js
@@ -0,0 +1,111 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="第一步">
+            修改 <Text style={styles.highlight}>App.js</Text> 来改变这个页面然后返回查看修改内容.
+          </Section>
+          <Section title="查看你的变更">
+            <ReloadInstructions />
+          </Section>
+          <Section title="调试">
+            <DebugInstructions />
+          </Section>
+          <Section title="学习更多">
+            阅读文档了解下一步要做什么:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
